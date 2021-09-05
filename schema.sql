e8ef62ed4d4994ca358518a1fe0c9090ed38bd97
Adam And <andadam24@gmail.com>
2021-09-05 19:05:12 -0400

    updated schema.sql

--- schema.sql
+++ schema.sql
@@ -3,3 +3,26 @@ DROP DATABASE IF EXISTS etracker_db;
 
 -- CREATE DATABASE
 CREATE DATABASE etracker_db;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT NOT NULL, 
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    PRIMARY KEY (id)
+);
